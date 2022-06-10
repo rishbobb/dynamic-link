3bb03167f1211040126a7520fd5cc371eec5e4ba
Rishabh <61441648+rishbobb@users.noreply.github.com>
2022-06-10 15:10:03 +0530

    Add ability to list dynlinks

--- index.js
+++ index.js
@@ -144,3 +144,9 @@ app.get("/edit", (req, res) => {
     });
   }
 });
+
+app.get("/list", (req, res) => {
+  db.all("SELECT name url deepurl FROM dynlinks", (err, rows) => {
+    res.json(rows);
+  });
+});
