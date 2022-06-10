b0eda1b82aa735c1d7565ac53dd082576985d994
Rishabh <61441648+rishbobb@users.noreply.github.com>
2022-06-10 15:14:34 +0530

    debug

--- index.js
+++ index.js
@@ -147,6 +147,7 @@ app.get("/edit", (req, res) => {
 
 app.get("/list", (req, res) => {
   db.all("SELECT name url deepurl FROM dynlinks", (err, rows) => {
+    console.log(rows);
     res.json({ rows });
   });
 });
